26f76c1423c64adfacc61a679654a98e4b09b65f
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-08 10:46:31 +0100

    added test for New

--- tracer_test.go
+++ tracer_test.go
@@ -1,11 +1,21 @@
 package trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
 	// go treats any function that starts with Test (taking a single *testing.T)
 	// argument as a unit test and will execute it when we run our tests
-	t.Error("No tests available. Yet!")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Return from New should not be nil")
+	} else {
+		tracer.Trace("Trace package is here.")
+		if buf.String() != "Trace package is here.\n" {
+			t.Errorf("Trace should not write '%s.'", buf.String())
+		}
+	}
 }
