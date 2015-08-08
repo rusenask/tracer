94c623d0e7656ff7aa639970263f25ecc55fe6e8
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-08 14:21:28 +0100

    output test

--- tracer.go
+++ tracer.go
@@ -1,7 +1,26 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 // Tracer is the interface that describes an object capable of
 // tracing events throughout the code.
 type Tracer interface {
 	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	// since Tracer is just an inferface we need tracer to return something real
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
 }
